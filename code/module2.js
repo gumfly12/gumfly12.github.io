c2d8a33806a68ac09ad13bec934d80ae72b6a5fc
gumfly12 <reddinosaur13@gmail.com>
2017-03-10 07:59:59 -0500

    Update module2.js

--- code/module2.js
+++ code/module2.js
@@ -1,3 +1,5 @@
 mouseX
 mouseY
 these camands allow you to have controle of what you just made with the mouse.
+because the mouse comand is used to controle what you made i had to write mousey when makeing the varieble 
+or else both the bear and the mouse would move at the same time insted of sepretly.
