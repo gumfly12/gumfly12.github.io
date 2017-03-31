804cef9fe22a32a2680a2868da0b77c1cebfaad5
gumfly12 <reddinosaur13@gmail.com>
2017-03-31 13:35:45 -0400

    Update m4.js

--- code/m4.js
+++ code/m4.js
@@ -1,1 +1,6 @@
-Waiting....
+This is the code for the deathtrooper who we temp change to panda.
+     image.src = "img/panda.png";
+        image.onload = function () {
+            ctx.drawImage(image, 0, 0, 64, 64,
+                          Math.floor(stfp.SCREEN_WIDTH * 0.2), 80, 64, 64)
+this code is curently broken because the sprite bugs out and takes damage when unhit.
